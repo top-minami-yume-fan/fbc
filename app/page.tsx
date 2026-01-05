17abac0e54c219734b0a15024f777dc5f6a4a4ab
jchen-sfu <jca476@sfu.ca>
2026-01-05 00:25:37 -0800

    site skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
 import Image from "next/image";
+import DummyContent from "@/components/DummyContent";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+    const dummy_content = "Fleetwood Badminton Club (FBC) was founded in 2016, and started with various school gyms in Surrey on flexible schedules. FBC strives to create a great environment to learn, train, and play the sport of badminton.  Badminton is a fun and exciting sport that boosts one's physical, mental, and social well-being.  FBC will provide a safe and healthy environment for children and adults to play a sport they love while encouraging the development of a positive attitude and lifestyle."
+    
+    return (
+        <div className="flex flex-col items-center p-10">
+            <div>
+                <h1>Welcome to Fleetwood Badminton Club!</h1>
+            </div>
+            <div>
+                <Image
+                    src="/images/placeholder.png"
+                    alt="Badminton Club"
+                    width={500}
+                    height={300}
+                />
+            </div>
+            <div>
+                About Us
+            </div>
+            <div>
+                <DummyContent content={dummy_content}/>
+            </div>
+            <div>
+                <Image 
+                    src="/images/flyer-wide.png"
+                    alt="Badminton Club 2"
+                    width={500}
+                    height={300}
+                />
+            </div>
+            <div>
+                <h3>Class Location</h3>
+                <ul>
+                    <li><b>Gym 1</b>: 8606 162 St, Surrey, BC </li>
+                    <li><b>Gym 2</b>: 8888 162 St, Surrey, BC </li>
+                    <li><b>Gym 3</b>: 15353 92 Ave, Surrey, BC </li>
+                    <li><b>Gym 4</b>: 1730 142 Street, Surrey,BC</li>
+                </ul>
+            </div>
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
